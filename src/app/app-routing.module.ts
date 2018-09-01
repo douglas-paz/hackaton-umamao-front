8646c8051dc7293a1703f08faca6aebedd1cdb38
Douglas Paz <douglas.paz@southsystem.com.br>
2018-09-01 19:34:06 -0300

    home screen beginnig

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
-import { MainComponent } from './modules/layout/main/main.component'
+import {HomeComponent} from './componentes/home/home.component';
+import {LoginComponent} from './components/login/login.component';
 const routes: Routes = [
-  { 
-    path: '' , component: MainComponent,
+  {
+    path: '' , component : HomeComponent,
   },
   {
     path: 'login', component: LoginComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { LayoutModule } from './modules/layout/layout.module';
+import { HomeComponent } from './componentes/home/home.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
